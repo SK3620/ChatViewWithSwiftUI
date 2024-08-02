c7c96aa1ea15640055162bc70c533785edc15d09
SK3620 <k.n.t11193620@gmail.com>
2024-08-02 18:52:23 +0900

    Add: リスト画面の大まかなレイアウトを作成

---
+++ ChatViewWithSwiftUI/Views/ListView.swift
@@ -0,0 +1,58 @@
+//
+//  ListView.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by 鈴木 健太 on 2024/08/02.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        VStack {
+            // Header
+            HStack {
+                Text("トーク")
+                    .font(.title2.bold())
+                
+                Spacer()
+                
+                HStack {
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Circle()
+                        .frame(width: 40, height: 40)
+                }
+            }
+            
+            // List
+            ScrollView {
+                VStack {
+                    ForEach(0..<5) {_ in
+                        HStack {
+                            Circle()
+                                .frame(width: 60, height: 60)
+                            VStack(alignment: .leading) {
+                                Text("タイトル")
+                                Text("最新のメッセージ")
+                                    .font(.footnote)
+                                    .foregroundColor(Color(uiColor: .secondaryLabel))
+                            }
+                            Spacer()
+                            Text("12/31")
+                                .font(.caption)
+                                .foregroundColor(Color(uiColor: .secondaryLabel))
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    ListView()
+}
