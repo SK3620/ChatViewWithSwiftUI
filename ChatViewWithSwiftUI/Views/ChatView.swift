844c0793121fe5a49b6e1468a3317db70cd594fa
SK3620 <k.n.t11193620@gmail.com>
2024-08-02 12:59:49 +0900

    Add: ナビゲーションエリアのUI作成

--- ChatViewWithSwiftUI/Views/ChatView.swift
+++ ChatViewWithSwiftUI/Views/ChatView.swift
@@ -43,7 +43,7 @@ extension ChatView {
     }
     
     private var inputArera: some View {
-        return  HStack {
+        return HStack {
             Circle()
                 .frame(width: 40, height: 40)
             Circle()
@@ -60,20 +60,20 @@ extension ChatView {
     }
     
     private var navigationArea: some View {
-        return   HStack {
-            Circle()
-                .frame(width: 40, height: 40)
+        return HStack {
+            Image(systemName: "chevron.backward")
+                .font(.title2)
             Text("Title")
+                .font(.title2.bold())
             Spacer()
-            Circle()
-                .frame(width: 40, height: 40)
-            Circle()
-                .frame(width: 40, height: 40)
-            Circle()
-                .frame(width: 40, height: 40)
+            HStack(spacing: 16) {
+                Image(systemName: "text.magnifyingglass")
+                Image(systemName: "phone")
+                Image(systemName: "line.3.horizontal")
+            }
+            .font(.title2)
         }
-            .foregroundColor(.white)
             .padding()
-            .background(.black.opacity(0.5))
+            .background(.cyan.opacity(0.9))
     }
 }
