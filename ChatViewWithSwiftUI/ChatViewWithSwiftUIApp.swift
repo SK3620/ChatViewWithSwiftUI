1d3be059ab8e5c7a50c725669ac74779d8e0d50c
SK3620 <k.n.t11193620@gmail.com>
2024-08-02 11:57:32 +0900

    Initial Commit

---
+++ ChatViewWithSwiftUI/ChatViewWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewWithSwiftUIApp.swift
+//  ChatViewWithSwiftUI
+//
+//  Created by 鈴木 健太 on 2024/08/02.
+//
+
+import SwiftUI
+
+@main
+struct ChatViewWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
